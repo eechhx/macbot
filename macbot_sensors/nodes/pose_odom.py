802a1302078d5f5b1f35ebed7a6c426168a05afa
eechhx <hsiaoy4@mcmaster.ca>
2021-01-23 13:34:54 -0500

    Optimized pose_odom node calculation

--- macbot_sensors/nodes/pose_odom.py
+++ macbot_sensors/nodes/pose_odom.py
@@ -6,6 +6,7 @@ import tf_conversions
 import tf2_ros
 import sys
 import signal
+from math import cos
 from nav_msgs.msg import Odometry
 from time import sleep
 from geometry_msgs.msg import PoseStamped, Quaternion, Point, Pose, Quaternion, Twist, Vector3
@@ -36,17 +37,13 @@ class poseOdom():
      def __init__(self):
           self.pubOdom = rospy.Publisher("odom", Odometry, queue_size = 2)
 
-          self.x_prev = 0.1
-          self.y_prev = 0.1
-          self.z_prev = 0.1
+          self.x_prev = 0.01
+          self.y_prev = 0.01
+          self.z_prev = 0.01
 
-          self.vx = 0.1
-          self.vy = 0.1
-          self.vz = 0.1
-
-          self.quat = [0.1, 0.1, 0.1, 0.1]
-          self.quat_prev = [0.1, 0.1, 0.1, 0.1]
-          self.delta_q = [0.1, 0.1, 0.1, 0.1]
+          self.quat = [0.01, 0.01, 0.01, 0.01]
+          self.quat_prev = [0.01, 0.01, 0.01, 0.01]
+          self.delta_q = [0.01, 0.01, 0.01, 0.01]
 
           self.time_now = rospy.Time.now()
           self.time_prev = rospy.Time.now()
@@ -61,22 +58,14 @@ class poseOdom():
 
      def callback(self, data):
           self.time_now = rospy.Time.now()
-          self.odom.header.stamp.secs = data.header.stamp.secs
-          self.odom.header.stamp.nsecs = data.header.stamp.nsecs
+          self.odom.header.stamp = rospy.Time.now()
 
           self.delta_t = (self.time_now - self.time_prev).to_sec()
-          self.delta_x = (data.pose.position.x + self.x_prev)/self.delta_t
-          self.delta_y = (data.pose.position.y + self.y_prev)/self.delta_t
-          self.delta_z = (data.pose.position.z + self.z_prev)/self.delta_t
 
           self.delta_q[0] = (data.pose.orientation.x + self.quat_prev[0])/self.delta_t
           self.delta_q[1] = (data.pose.orientation.y + self.quat_prev[1])/self.delta_t
           self.delta_q[2] = (data.pose.orientation.z + self.quat_prev[2])/self.delta_t
           self.delta_q[3] = (data.pose.orientation.w + self.quat_prev[3])/self.delta_t
-
-          self.x_prev = data.pose.position.x
-          self.y_prev = data.pose.position.y
-          self.z_prev = data.pose.position.z
 
           self.quat_prev[0] = data.pose.orientation.x
           self.quat_prev[1] = data.pose.orientation.y
@@ -85,8 +74,11 @@ class poseOdom():
 
           self.rpy = tf.transformations.euler_from_quaternion(self.delta_q)
 
+          self.vx = ((data.pose.position.x + self.x_prev)/self.delta_t)/cos(self.rpy[2])
+          self.x_prev = data.pose.position.x
+
           self.odom.pose.pose = Pose(Point(data.pose.position.x, data.pose.position.y, data.pose.position.z), Quaternion(data.pose.orientation.x, data.pose.orientation.y, data.pose.orientation.z, data.pose.orientation.w))
-          self.odom.twist.twist = Twist(Vector3(self.delta_x, self.delta_y, self.delta_z), Vector3(self.rpy[0], self.rpy[1], self.rpy[2]))
+          self.odom.twist.twist = Twist(Vector3(self.vx, 0, 0), Vector3(0, 0, self.rpy[2]))
 
           # Publish message
           self.pubOdom.publish(self.odom)
